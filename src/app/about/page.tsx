647ea24d2f92b090d14700d4c372cf4c440ef76f
Devin Rider <42556432+Squiege@users.noreply.github.com>
2025-02-11 08:49:20 -0800

    Updated about.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,14 @@
 export default function About() {
     return (
     <div>
-        <h1>To Do</h1>
-        <h1>Completed</h1>
+        <h1 className="text-3xl text-center mt-6">About</h1>
+        <p className="text-lg mt-6 text-center">This is a simple To-Do List Tracker App built with Next.js and Tailwind CSS. 
+            With this app, you can easily manage your daily tasks and stay organized. 
+            You can add new tasks, mark them as completed, and delete tasks that are no longer needed. 
+            The app provides a clean and intuitive user interface, making it easy to keep track of your to-do list. 
+            Additionally, it is responsive and works seamlessly on both desktop and mobile devices.</p>
+        <br />
+        <p className="text-center">Find more apps created by Devin Rider <a href="https://github.com/Squiege" className="underline text-blue-500">here</a></p>
     </div>
     
     );
